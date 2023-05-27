0b691e92b361a469ec0ad69646ca832f531d4d17
jacksmithinsulander <666.jack.smith@protonmail.com>
2023-05-27 21:41:28 +0200

    Made a framwork for what is needed to write

--- src/models/Wallet.js
+++ src/models/Wallet.js
@@ -1,18 +1,6 @@
-const openpgp = require("openpgp");
 
 class Wallet {
 	async generateKeyPair(userName, password) {
-		const { privateKey, publicKey, revocationCertificate } = await openpgp.
-			generateKey({
-				type: "ecc",
-				curve: "curve25519",
-				userIDs: [{ name: userName }],
-				passphrase: password,
-				format: "armored"
-			});
-		console.log(privateKey);
-		console.log(publicKey);
-		console.log(revocationCertificate);
 	}
 
 	async signTransaction() {
