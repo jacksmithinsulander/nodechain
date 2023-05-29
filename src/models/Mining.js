d10b6e5bf4f078305abbf609e2354a602d7ff9a2
jacksmithinsulander <666.jack.smith@protonmail.com>
2023-05-29 16:47:10 +0200

    Added a mining algorithm but havnt had time to try it yet, need to implement oit to the block function

--- src/models/Mining.js
+++ src/models/Mining.js
@@ -1,11 +1,9 @@
 class Mining {
 	constructor() {
-		this.miningWords = [
-			"lfg", "ngmi", "wagmi", "420", "666", "1337"
-		]:
+		this.miningWords = ["lfg", "ngmi", "wagmi", "420", "666", "1337"];
 	}
 
-	const sortingAlgo = (a, b) = {
+	sortingAlgo(a, b) {
 		const valueA = a.gasFee;
 		const valueB = b.gasFee;
 		if (valueA < valueB) {
@@ -14,14 +12,29 @@ class Mining {
 		if (valueA > valueB) {
 			return -1;
 		}
-		return 0
+		return 0;
 	}
 
-	chooseTransaction(mempool) {
+	async chooseTransaction(mempool, lastHash) {
 		const mempoolArr = mempool.printMempool();
-		mempoolArr.sort(sortingAlgo);
-		const choosenTransactions = mempoolArr.slice(0, 5);
-
+		mempoolArr.sort(this.sortingAlgo);
+		const data = mempoolArr.slice(0, 5);
+		let conditionMet = false;
+		let hash;
+		while (!conditionMet) {
+			for (let i = 0; i < this.miningWords.length; i++) {
+				const pattern = new RegExp(this.miningWords[i], "i");
+				let timestamp = Date.now();
+				let nonce = Math.floor(Math.random() * 100) + 1;
+				hash = await hashInstance.
+					generateHash(timestamp, lastHash, data, nonce);
+				if (pattern.test(hash)) {
+					conditionMet = true;
+					break;
+				}
+			}
+		}
+		return hash;
 	}
 }
 
