72946d0fbc0e8bf0d9a3e09f6c86250e9cc7b5d5
jacksmithinsulander <666.jack.smith@protonmail.com>
2023-05-29 19:49:49 +0200

    I think the blockchain is kind of done, will just implement a controller function

--- src/models/Block.js
+++ src/models/Block.js
@@ -1,13 +1,16 @@
+const Mining = require(".Mining");
 const Hash = require("./Hash");
 const GENESIS_DATA = require("./genesisBlock");
 
 const hashInstance = new Hash();
+const mining = new Mining();
 
 class Block {
-	constructor({ timestamp, data, nonce, hash, lastHash }) {
+	constructor({ timestamp, data, nonce, index, hash, lastHash }) {
 		this.timestamp = timestamp;
 		this.data = data;
 		this.nonce = nonce;
+		this.index = index;
 		this.hash = hash;
 		this.lastHash = lastHash;
 	}
@@ -16,17 +19,26 @@ class Block {
 		return new this(GENESIS_DATA);
 	}
 
-	static async mineBlock({ lastBlock, data }) {
-		const timestamp = Date.now();
+	static async mineBlock({ lastBlock, mempool }) {
+		//const timestamp = Date.now();
 		const lastHash = lastBlock.hash;
-		let nonce = lastBlock.nonce + 1;
-		let hash = await hashInstance.
-			generateHash(timestamp, lastHash, data, nonce);
+		let index = lastBlock.index + 1;
+		//let hash = await hashInstance.
+		//	generateHash(timestamp, lastHash, data, nonce);
 
+		const minedBlock = await mining.
+			chooseTransaction(mempool, lastHash, index)
+			.then(returnData => {
+				const hash = returnData.hash;
+				const timestamp = returnData.timestamp;
+				const nonce = returnData.nonce;
+			});
+		
 		const minedBlock = new this({
 			timestamp,
 			lastHash,
 			nonce,
+			index,
 			data,
 			hash,
 		});
--- src/models/Mining.js
+++ src/models/Mining.js
@@ -15,26 +15,33 @@ class Mining {
 		return 0;
 	}
 
-	async chooseTransaction(mempool, lastHash) {
+	async chooseTransaction(mempool, lastHash, index) {
 		const mempoolArr = mempool.printMempool();
 		mempoolArr.sort(this.sortingAlgo);
 		const data = mempoolArr.slice(0, 5);
 		let conditionMet = false;
-		let hash;
+		let hash, timestamp, nonce;
 		while (!conditionMet) {
 			for (let i = 0; i < this.miningWords.length; i++) {
 				const pattern = new RegExp(this.miningWords[i], "i");
 				let timestamp = Date.now();
 				let nonce = Math.floor(Math.random() * 100) + 1;
 				hash = await hashInstance.
-					generateHash(timestamp, lastHash, data, nonce);
+					generateHash(timestamp, lastHash, nonce, index, data);
 				if (pattern.test(hash)) {
 					conditionMet = true;
 					break;
 				}
 			}
 		}
-		return hash;
+		const returnData = {
+			hash: hash,
+			timestamp: timestamp,
+			nonce: nonce;
+			data: data
+		}
+		
+		return returnData;
 	}
 }
 
