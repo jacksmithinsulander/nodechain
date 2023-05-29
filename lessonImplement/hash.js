78604532597833fa2399367bb8d41917fb171ca4
jacksmithinsulander <666.jack.smith@protonmail.com>
2023-05-29 19:52:57 +0200

    Removed the versions from the lessons since I am satisfied enough with my own version

--- lessonImplement/hash.js
+++
@@ -1,9 +0,0 @@
-const crypto = require("crypto");
-
-const createHash = (...args) => {
-	const hash = crypto.createHash("sha256");
-	hash.update(args.sort().join(' '));
-	return hash.digest("hex");
-}
-
-module.exports = createHash
