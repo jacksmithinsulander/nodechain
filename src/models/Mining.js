f54c7548b6e26247b211120989b15a59b5153ca2
jacksmithinsulander <666.jack.smith@protonmail.com>
2023-06-06 14:45:00 +0200

    Implemented miner rewards

--- src/models/Mining.js
+++ src/models/Mining.js
@@ -25,74 +25,80 @@ class Mining {
     return 0;
   }
 
-  async chooseTransaction(wallet, mempool, lastHash, index) {
-    if (!wallet) {
-      throw new Error("No wallet found");
-    }
-    const mempoolArr = await mempool;
-    console.log("Mempool: ", mempool, "Last Hash: ", lastHash, "Index: ", index);
-    mempoolArr.sort(this.sortingAlgo);
-    const data = mempoolArr.slice(0, 5);
-
-    for (const transaction of data) {
-      const index = mempoolArr.indexOf(transaction);
-      if (index > -1) {
-        mempool.splice(index, 1);
-      }
-    }
-
-    let conditionMet = false;
-    let hash, timestamp, nonce, usedWord;
-    let iteration = 1;
-
-    while (!conditionMet) {
-      for (let i = 0; i < this.miningWords.length; i++) {
-        const pattern = new RegExp(this.miningWords[i], "i");
-        timestamp = Date.now();
-        nonce = this.previousNonce + ((iteration - 1) % 3) + 1; // Nonce within the range 1-3
-        usedWord = this.miningWords[i];
-        hash = await hashInstance.generateHash(timestamp, lastHash, nonce, index, data);
-        console.log("Iteration: ", iteration);
-        console.log("Tested Hash: ", hash);
-        if (pattern.test(hash)) {
-          conditionMet = true;
-          break;
-        }
-      }
-      iteration++;
-    }
-
-    this.previousNonce = nonce + 1; // Increment the previousNonce by 1
-
-    console.log("Used word is", usedWord);
-    const miningReward = usedWord.length - 1;
-    const returnData = {
-      hash: hash,
-      timestamp: timestamp,
-      nonce: nonce,
-      data: data,
-      miner: wallet.publicKey,
-      miningReward: miningReward,
-    };
-    console.log("Mining reward is :", miningReward);
-
-    console.log(wallet);
-
-    const signature = wallet.signTransaction(returnData);
-    const isValidSignature = wallet.verifySignature(returnData, signature, wallet.publicKey);
-    if (!isValidSignature) {
-      throw new Error("Invalid signature");
-    }
-
-    const walletBal = wallet.checkWalletBalance(wallet);
-    const newWalletBal = walletBal + miningReward;
-
-    wallet.updateBalance(wallet.publicKey, newWalletBal);
-
-    returnData.signature = signature;
-
-    return returnData;
+async chooseTransaction(wallet, mempool, lastHash, index) {
+  if (!wallet) {
+    throw new Error("No wallet found");
   }
+  const mempoolArr = await mempool;
+  console.log("Mempool: ", mempool, "Last Hash: ", lastHash, "Index: ", index);
+  mempoolArr.sort(this.sortingAlgo);
+  const data = mempoolArr.slice(0, 5);
+
+  for (const transaction of data) {
+    const index = mempoolArr.indexOf(transaction);
+    if (index > -1) {
+      mempool.splice(index, 1);
+    }
+  }
+
+  let conditionMet = false;
+  let hash, timestamp, nonce, usedWord;
+  let iteration = 1;
+  let totalGasFee = 0; // Variable to hold the total gas fee
+
+  // Calculate the total gas fee of all transactions
+  for (const transaction of data) {
+    totalGasFee += transaction.gasFee;
+  }
+
+  while (!conditionMet) {
+    for (let i = 0; i < this.miningWords.length; i++) {
+      const pattern = new RegExp(this.miningWords[i], "i");
+      timestamp = Date.now();
+      nonce = this.previousNonce + ((iteration - 1) % 3) + 1; // Nonce within the range 1-3
+      usedWord = this.miningWords[i];
+      hash = await hashInstance.generateHash(timestamp, lastHash, nonce, index, data);
+      console.log("Iteration: ", iteration);
+      console.log("Tested Hash: ", hash);
+      if (pattern.test(hash)) {
+        conditionMet = true;
+        break;
+      }
+    }
+    iteration++;
+  }
+
+  this.previousNonce = nonce + 1; // Increment the previousNonce by 1
+
+  console.log("Used word is", usedWord);
+  const miningReward = (usedWord.length - 1) + (totalGasFee * 0.1); // Add 10% of total gas fee to mining reward
+  const returnData = {
+    hash: hash,
+    timestamp: timestamp,
+    nonce: nonce,
+    data: data,
+    miner: wallet.publicKey,
+    miningReward: miningReward,
+  };
+  console.log("Mining reward is :", miningReward);
+
+  console.log(wallet);
+
+  const signature = wallet.signTransaction(returnData);
+  const isValidSignature = wallet.verifySignature(returnData, signature, wallet.publicKey);
+  if (!isValidSignature) {
+    throw new Error("Invalid signature");
+  }
+
+  const walletBal = wallet.checkWalletBalance(wallet);
+  const newWalletBal = walletBal + miningReward;
+
+  wallet.updateBalance(wallet.publicKey, newWalletBal);
+
+  returnData.signature = signature;
+
+  return returnData;
+}
 }
 
 module.exports = Mining;
